7f8def8bc35ff339df5ac831361d5d6c80e78407
Krzysztof Kołodziej <krzysztof.kolodzie2212@gmail.com>
2022-11-14 10:01:10 +0100

    second commit

--- src/main/java/pl/lotto/LottoGameApplication.java
+++ src/main/java/pl/lotto/LottoGameApplication.java
@@ -1,10 +1,56 @@
 package pl.lotto;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+
 public class LottoGameApplication {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
+
         LottoNumberGenerator lottoNumberGenerator = new LottoNumberGenerator();
-        double random = lottoNumberGenerator.generateRandomNumber();
+        ArrayList random = lottoNumberGenerator.generateRandomNumber();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Proszę podać 6 liczb od 1 do 99");
+
+        ArrayList<Integer> listaUzytkownik =new ArrayList<Integer>();
+
+        for (int i=0; i<6; i++){
+            int s = Integer.parseInt(reader.readLine());
+            if(s<=0 || s>99){
+                System.out.println("Liczba musi znajdować się w przedziale od 1 do 99");
+                break;
+            }
+            listaUzytkownik.add(s);
+        }
+
+        int iloscTrafien =0;
+
+        for (Integer s: listaUzytkownik
+             ) {
+            for (int i=0; i<6; i++){
+                if (random.get(i) ==s){
+                    iloscTrafien++;
+                }
+
+            }
+        }
+        System.out.println("Wylosowane liczby: ");
+
+       for (int i=0; i<6; i++){
+           System.out.println(random.get(i));
+       }
+
+        System.out.println("Ilość trafień "+iloscTrafien);
+
+        if (iloscTrafien >=3){
+            System.out.println("Wygrałeś");
+        }
+        else System.out.println("Przegrałeś");
     }
 
 }
--- src/main/java/pl/lotto/LottoNumberGenerator.java
+++ src/main/java/pl/lotto/LottoNumberGenerator.java
@@ -1,7 +1,21 @@
 package pl.lotto;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 public class LottoNumberGenerator {
-    public double generateRandomNumber(){
-        return Math.random();
+    public ArrayList<Integer> generateRandomNumber(){
+
+        int min = 1;
+        int max = 99;
+
+        ArrayList<Integer> listaRandom =new ArrayList<Integer>();
+
+        for (int i=1; i<=6; i++) {
+            int random = (int) Math.floor(Math.random() * (max - min + 1) + min);
+            listaRandom.add(random);
+        }
+        return listaRandom;
     }
+
 }
